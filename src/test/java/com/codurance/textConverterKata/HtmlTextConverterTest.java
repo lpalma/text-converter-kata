fcffdd9f85df8059baa9e05e95877e645cea2325
Luciano Palma <lucianopalma90@gmail.com>
2017-07-25 15:47:34 +0100

    initialize project

---
+++ src/test/java/com/codurance/textConverterKata/HtmlTextConverterTest.java
@@ -0,0 +1,13 @@
+package com.codurance.textConverterKata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HtmlTextConverterTest {
+    @Test
+    public void foo() {
+        HtmlTextConverter converter = new HtmlTextConverter("foo");
+        assertEquals("fixme", converter.getFilename());
+    }
+}
