3bb99a911db6d99d0b8d4a3cc16e54db1852e247
Luciano Palma <lucianopalma90@gmail.com>
2017-07-25 16:01:53 +0100

    remove unnecessary test

--- src/test/java/com/codurance/textConverterKata/HtmlTextConverterShould.java
+++ src/test/java/com/codurance/textConverterKata/HtmlTextConverterShould.java
@@ -5,9 +5,4 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HtmlTextConverterShould {
-    @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
-    }
 }
